d1384dfb5aeae424ea9646d106d95ab9d18cd584
jiwonchoe <jiwonkun@gmail.com>
2013-10-29 11:32:57 +0900

    Update listWindow.py

--- listWindow.py
+++ listWindow.py
@@ -6,20 +6,79 @@ class listWindow(QtGui.QWidget):
         super(listWindow, self).__init__()
 
         mainLayout = QtGui.QVBoxLayout()
-        self.listWg = QtGui.QListWidget()
-        button = QtGui.QPushButton('alpha')
+        buttonVLayout = QtGui.QVBoxLayout()
+        buttonLayout01 = QtGui.QHBoxLayout()
+        buttonLayout02 = QtGui.QHBoxLayout()
+        buttonLayout03 = QtGui.QHBoxLayout()
 
-        button.clicked.connect(self.run)
+        # layout main add
+        self.listWg = QtGui.QListWidget()
+        self.outPathLineEditer = QtGui.QLineEdit('d:/attr.mel')
+
+        # layout 1 add
+        runButton = QtGui.QPushButton('select Check')
+        lineEditer = QtGui.QLineEdit()
+        filterButton = QtGui.QPushButton('filter')
+
+        # layout 2 add
+        exportButton = QtGui.QPushButton('all OutputFile')
+        selectExportButton = QtGui.QPushButton('select OutputFile')
+
+        # layout 3 add
+
+        self.radioButton01 = QtGui.QRadioButton('translate', self)
+        self.radioButton02 = QtGui.QRadioButton('rotate', self)
+        self.radioButton03 = QtGui.QRadioButton('scale', self)
+        self.radioButton04 = QtGui.QRadioButton('visibility', self)
+
+        self.radioButton01.setAutoExclusive(0)
+        self.radioButton02.setAutoExclusive(0)
+        self.radioButton03.setAutoExclusive(0)
+        self.radioButton04.setAutoExclusive(0)
+        self.radioButton01.setChecked(1)
+        self.radioButton02.setChecked(1)
+        self.radioButton03.setChecked(1)
+        self.radioButton04.setChecked(1)
+
+        # connect
+        
+        exportButton.clicked.connect(self.run)
 
         self.listWg.addItems(['append', 'bend', 'cycle', 'system', 'code', 'mac', 'python'])
         self.listWg.setSelectionMode(QtGui.QAbstractItemView.MultiSelection)
 
+        buttonLayout01.addWidget(runButton)
+        buttonLayout01.addWidget(lineEditer)
+        buttonLayout01.addWidget(filterButton)
+
+        buttonLayout02.addWidget(exportButton)
+        buttonLayout02.addWidget(selectExportButton)
+
+        buttonLayout03.addWidget(self.radioButton01)
+        buttonLayout03.addWidget(self.radioButton02)
+        buttonLayout03.addWidget(self.radioButton03)
+        buttonLayout03.addWidget(self.radioButton04)
+
         mainLayout.addWidget(self.listWg)
-        mainLayout.addWidget(button)
+        mainLayout.addWidget(self.outPathLineEditer)
+
+
+        buttonVLayout.addLayout(buttonLayout01)
+        buttonVLayout.addLayout(buttonLayout02)
+        buttonVLayout.addLayout(buttonLayout03)
+
+        mainLayout.addLayout(buttonVLayout)
 
         self.setLayout(mainLayout)
         self.setWindowTitle('List Window')
+
         self.setGeometry(300, 300, 200, 500)
+
+        styleFile = open('w:/BBM/Assets/Rig/Set/Rigging/set/darkorange.stylesheet', 'r')
+        self.setStyleData = styleFile.read()
+        styleFile.close()
+        self.setStyleSheet(self.setStyleData)
+
         self.show()
     def run(self):
         getList = self.listWg.selectedItems()
@@ -33,4 +92,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
